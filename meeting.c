13eb7b9e79fdf851016e4733e7f8265fee551d6e
YashaswiRavikumar <58076484+YashaswiRavikumar@users.noreply.github.com>
2020-03-03 21:05:28 +0530

    Update meeting.c

--- meeting.c
+++ meeting.c
@@ -1,63 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-void printMaxmtgs(int s[], int f[], int n) 
-{ 
-    int i, j; 
-  
-    printf ("Following mtgs are selected n : "); 
-
-    i = 0; 
-    printf("%d ", i); 
-  
-
-    for (j = 1; j < n; j++) 
-    { 
-
-      if (s[j] >= f[i]) 
-      { 
-          printf ("%d ", j); 
-          i = j; 
-      } 
-    } 
-} 
-
-void insertsort(int arr[], int arr2[], int n) 
-{	int i, key, j; 
-	for (i = 1; i < n; i++) 
-	{	key = arr[i]; 
-		j = i - 1; 
-		while (j >= 0 && arr[j] > key) 
-		{	arr[j + 1] = arr[j];
-			arr2[j] = arr2[j] + arr2[j+1] ;
-			arr2[j+1] = arr2[j] - arr2[j+1] ;
-			arr2[j] = arr2[j] - arr2[j+1] ;
-			j = j - 1; 
-		} 
-		arr[j + 1] = key; 
-	} 
-} 
-
-void print(int arr[],int arr2[], int n) 
-{	int i; 
-	for (i = 0; i < n; i++) 
-	{ printf("{%d,%d} ", arr2[i], arr[i]);
-	} 
-	printf("\n"); 
-} 
-
-
-int main() 
-{ 
-    int s[10] , f[10] ; 
-	for (int i=0 ; i<10 ; i++)
-	{	s[i] = rand()%20;
-		f[i] = rand()%20;
-	}
-    int n = sizeof(s)/sizeof(s[0]);
-	insertsort(f, s, n); 
-	print(f,s,n);
-    printMaxmtgs(s, f, n); 
-    return 0; 
-} 
+#include<stdio.h>
+int main()
+{
+        int a[10],b[10],n,count=0;
+        printf("Enter the number of meetings\n");
+        scanf("%d",&n);
+        printf("Enter the start time of the meetings\n");
+        for(int i=0;i<n;i++)
+        {
+                scanf("%d",&a[i]);
+        }
+        printf("Enter the end time of the meetings\n");
+        for(int i=0;i<n;i++)
+        {
+                scanf("%d",&b[i]);
+        }
+        for(int i=0;i<n;i++)
+        {
+                for(int j=0;j<n;j++)
+                {
+                        if(b[i]<b[j])
+                        {
+                                int temp=b[i];
+                                b[i]=b[j];
+                                b[j]=temp;
+                                int temp1=a[i];
+                                a[i]=a[j];
+                                a[j]=temp1;
+                        }
+                 }
+         }
+         printf("\n");
+         printf("The meetings that can be conducted are\n");
+         for(int i=0;i<n;i++)
+         {
+                if((b[i]==a[i+1])||(b[i]<a[i+1])||(b[i]>a[i+1]&&a[i]>a[i+1]))
+                {
+                        printf("%d to %d\n",a[i],b[i]);
+                        count++;
+                }
+         }
+         printf("\nThe number of meetings that can be conducted is %d\n",count);
+}
